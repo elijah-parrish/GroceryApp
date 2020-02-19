644f000e7832d4a01e86a5f5b28ff101f88462e9
Elijah Parrish <elijah.parrish321@gmail.com>
2020-02-19 12:55:26 -0500

    Fixed typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = requre('path');
+const path = require('path');
 const items = require('./routes/api/items');
 const app = express(); // The express application (traditionally called app)
 
