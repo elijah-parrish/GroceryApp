b10078e0a330aee91a8a08a871e9137bc6ad2a0d
Elijah Parrish <elijah.parrish321@gmail.com>
2020-02-19 12:17:30 -0500

    Deploying to heroku

---
+++ client/src/components/itemModal.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import {
+    Button,
+    Modal, 
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from 'reactstrap';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+
+class ItemModal extends Component {
+    state = {
+        modal: false, // represents if the modal is open or not
+        name: '',
+        cost: ''
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        })
+    }
+
+    onChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        const newItem = {
+            name: this.state.name,
+            cost: this.state.cost
+            // TODO add nutrition data, price, etc here...
+        }
+
+        //Add item vie addItem action
+        this.props.addItem(newItem);
+
+        //Close the modal
+        this.toggle();
+    }
+
+    render() {
+        return(
+            <div>
+                <Button
+                color = "dark"
+                style = {{marginBottom: '2rem'}}
+                onClick = {this.toggle}
+                >Add Item</Button>
+
+                <Modal
+                isOpen = {this.state.modal}
+                toggle = {this.toggle}>
+                    <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.onSubmit}>
+                            <FormGroup>
+                                <Label for='item'>New Item</Label>
+
+                                <Input 
+                                style = {{marginBottom: '1em'}}
+                                type="text"
+                                name="name"
+                                id="item"
+                                placeholder="Add Shopping item"
+                                onChange={this.onChange}
+                                />
+
+                                <Input 
+                                style = {{marginBottom: '1em'}}
+                                type="text"
+                                name="cost"
+                                id="cost"
+                                placeholder="Item price"
+                                onChange={this.onChange}
+                                />
+
+                                <Button
+                                color="dark"
+                                style={{marginTop: '2rem'}}
+                                block
+                                >Add Item</Button>
+
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    item: state.item
+})
+
+export default connect(mapStateToProps, { addItem })(ItemModal);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = requre('path');
 const items = require('./routes/api/items');
 const app = express(); // The express application (traditionally called app)
 
@@ -21,6 +21,14 @@ mongoose
 // Use routes
 app.use('/api/items', items);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV == 'production') {
+    // Set static folder
+    app.use(express.static('/client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 // process.env.PORT is used for Heroku (later on)
 const port = process.env.PORT || 5000;
 
